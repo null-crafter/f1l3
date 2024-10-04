05c3e07fefc738267823e4f5c770afa33dd544ef
null crafter <163591881+null-crafter@users.noreply.github.com>
2024-10-04 05:52:05 +0000

    format code

--- src/f1l3/core/views.py
+++ src/f1l3/core/views.py
@@ -1,14 +1,17 @@
 from django.http import FileResponse
+from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 
 from core.models import UploadedFile
 from core.serializers import UploadedFileSerializer
-from django.shortcuts import render
+
 
 def home(request):
     return render(request, "core/index.html")
+
+
 @api_view(["POST"])
 def upload(request):
     serializer = UploadedFileSerializer(data=request.data)
@@ -16,13 +19,18 @@ def upload(request):
     instance = serializer.save()
     return Response({"alias": instance.alias})
 
+
 @api_view(["POST"])
 def upload_html(request):
     data = request.data
     serializer = UploadedFileSerializer(data=data)
     serializer.is_valid()
     instance = serializer.save()
-    return render(request, "core/uploaded.html", {"filename": data["file"].name, "alias": instance.alias})
+    return render(
+        request,
+        "core/uploaded.html",
+        {"filename": data["file"].name, "alias": instance.alias},
+    )
 
 
 @api_view(["GET"])
